c7afa79f38a0709fa763936398733c53929ba83c
Porthos <leedeffebach@gmail.com>
2019-04-03 15:00:02 -0400

    Almost thre - stuck on cant add first two terms

--- poly.ml
+++ poly.ml
@@ -17,6 +17,55 @@ type pExp =
 
 
 
+(* 
+  Compute degree of a polynomial expression.
+
+  Hint 1: Degree of Term(n,m) is m
+  Hint 2: Degree of Plus[...] is the max of the degree of args
+  Hint 3: Degree of Times[...] is the sum of the degree of args 
+*)
+let rec degree (_e:pExp): int =
+  match _e with
+    | Term(n, m) -> m
+    | Plus(l) -> get_degree_plus l 0
+    | Times(l) -> get_degree_times l 0
+
+and get_degree_plus (el:pExp list) (hi:int) : int = 
+  match el with
+    | [] -> hi
+    | hd::tl ->
+      let current = degree hd in
+      if(current > hi ) then
+        get_degree_plus tl current
+      else
+        get_degree_plus tl hi
+
+and get_degree_times (el:pExp list) (hi:int) : int = 
+  match el with
+    | [] -> hi
+    | hd::tl ->
+      let current = degree hd in
+      let hi = current + hi in
+        get_degree_times tl hi
+;;
+
+(* 
+  Comparison function useful for sorting of Plus[..] args 
+  to "normalize them". This way, terms that need to be reduced
+  show up one after another.
+  *)
+let compare (e1: pExp) (e2: pExp) : int =
+  if( degree e1 > degree e2 ) then -1
+  else if(degree e1 < degree e2) then 1
+  else 0
+;;
+
+let sort_by_degree (p: pExp list) : pExp list =
+  List.sort compare p
+;;
+
+
+
 (*
   Function to traslate betwen AST expressions
   to pExp expressions
@@ -38,17 +87,21 @@ let rec from_expr (_e: Expr.expr) : pExp =
         let t1 = from_expr e1 in
         let t2 = from_expr e2 in
           let l = t1::t2::[] in
+          let l = sort_by_degree l in
             Plus(l)
       | Sub(e1, e2) ->
         let t1 = from_expr e1 in
         let t2 = from_expr e2 in
         let n = Term(-1, 0) in
           let n1 = Times(n::t2::[]) in
-            Plus(t1::n1::[])
+            let l = t1::n1::[] in
+            let l = sort_by_degree l in
+            Plus(l)
       | Mul(e1, e2) ->
         let t1 = from_expr e1 in
         let t2 = from_expr e2 in
           let l = t1::t2::[] in
+          let l = sort_by_degree l in
             Times(l)
       | Pow(e1, i) ->
         (
@@ -75,35 +128,6 @@ let rec from_expr (_e: Expr.expr) : pExp =
 ;;
 
 
-
-(* 
-  Compute degree of a polynomial expression.
-
-  Hint 1: Degree of Term(n,m) is m
-  Hint 2: Degree of Plus[...] is the max of the degree of args
-  Hint 3: Degree of Times[...] is the sum of the degree of args 
-*)
-(* TODO *)
-let degree (_e:pExp): int =
-  match _e with
-    | Term(n, m) -> m
-    | Plus(l) -> 0
-    | Times(l) -> 0
-;;
-
-
-
-(* 
-  Comparison function useful for sorting of Plus[..] args 
-  to "normalize them". This way, terms that need to be reduced
-  show up one after another.
-  *)
-let compare (e1: pExp) (e2: pExp) : bool =
-  degree e1 > degree e2
-;;
-
-
-
 (* Print a pExpr nicely 
   Term(3,0) -> 3
   Term(5,1) -> 5x 
@@ -114,7 +138,7 @@ let compare (e1: pExp) (e2: pExp) : bool =
   Hint 1: Print () around elements that are not Term() 
   Hint 2: Recurse on the elements of Plus[..] or Times[..]
 *)
-(* TODO - Loop times and plus so dont have () arround every single one, low priority*)
+(* TODO - Loop times and plus so dont have () arround every single one, low priority *)
 let rec print_pExp (_e: pExp): unit =
   match _e with
     | Term(m, n) ->
@@ -131,24 +155,29 @@ let rec print_pExp (_e: pExp): unit =
     | Plus(l) ->
       (
       match l with
-        | h::t::[] ->
+        | h::t::li ->
           Printf.printf("(");
           print_pExp h;
           Printf.printf("+");
           print_pExp t;
           Printf.printf(")");
-        | _ -> Printf.printf "";
+          let p = Plus(li) in
+            print_pExp p;
+        | h::[] ->
+          print_pExp h;
+        | [] ->
+          Printf.printf "1";
       )
     | Times(l) ->
       (
         match l with
-          | h::t::[] ->
+          | h::t::li ->
             Printf.printf("(");
             print_pExp h;
             Printf.printf("*");
             print_pExp t;
             Printf.printf(")");
-          | _ -> Printf.printf ""
+          | _ -> Printf.printf "2"
       )
 ;;
 
@@ -172,13 +201,32 @@ let rec print_pExp (_e: pExp): unit =
   Hint 6: Find other situations that can arise
 *)
 
-let rec commutative (t:pExp) (p:pExp list) (l: pExp list): pExp list=
+let rec distributive_term (t:pExp) (p:pExp list) (l: pExp list): pExp list=
   match p with
   | hd::tl ->
     let ti = Times(t::hd::[]) in
       let l = ti::l in
-        commutative t tl l
+        distributive_term t tl l
   | [] -> l
+  ;;
+
+let rec inner_loop (p: pExp) (el: pExp list) (l: pExp list) : pExp list =
+  match el with
+  | [] -> l
+  | hd::tl ->
+    let t = Times(p::hd::[]) in
+    let l = t::l in
+      inner_loop p tl l
+;;
+
+let rec distributive_plus (el1:pExp list) (el2:pExp list) (l: pExp list): pExp list=
+  match el1 with
+  | [] -> l
+  | hd::tl ->
+    let l1 = [] in
+    let t = inner_loop hd el2 l1 in
+    let l = t@l in
+      distributive_plus tl el2 l
   ;;
 
 (* NOTE: The labels for all names go Outer expression type - Head type - second element type*)
@@ -195,51 +243,73 @@ let rec simplify1 (e:pExp): pExp =
                   | Term(m1, n1) ->
                     (
                       match tl with
-                        | Term(m2, n2) -> (* Plus - Term - Term *) (* If terms are same exp add, otherwise return plus list, if no more in plus list return term, else return list *)
+                        | Term(m2, n2) -> (* Plus - Term - Term *)
                           if(n1 = n2) then
                             let t = Term(m1 + m2, n1) in
                             (
                               match li with
                                | [] -> t
-                               | _ -> Plus(t::li)
+                               | _ ->
+                                  let l = t::li in
+                                  let l = sort_by_degree l in
+                                    Plus(l)
                             )
-                          else
-                            Plus(hd::tl::li) (* Figure out how to go to next ones in cant*)
+                          else (* TODO - Figure out how to go to next ones in cant add first two scenario - simplify li*)
+                            Plus(hd::tl::li)
+                            (*let l = tl::li in
+                            let l = sort_by_degree l in
+                              let p = Plus(l) in
+                              let s = simplify1 p in
+                                Plus(hd::s::[])*)
                         | Plus(el) -> (* Plus - Term - Plus *) (* Flatten *)
-                            let newList = hd::el@li in
-                              Plus(newList)
+                            let l = hd::el@li in
+                            let l = sort_by_degree l in
+                              Plus(l)
                         | Times(el) -> (* Plus - Term - Times *) (* Simplify tail and put into plus list *)
                           let tl = simplify1 tl in
-                          let e2 = Plus(hd::tl::li) in
+                          let l = hd::tl::li in
+                          let l = sort_by_degree l in
+                          let e2 = Plus(l) in
                             simplify1 e2
                     )
 
                   | Plus(el) ->
                     (
                     match tl with
-                      | Term(m, n) -> (* Plus - Plus - Term *) (* Flatten - FIX*)
-                        let newList = el@tl::li in
-                          Plus(newList)
-                      | Plus(el2) -> (* Plus - Plus - Plus *)
-                        Plus(el@el2@li)
-                      | Times(el2) -> (* Plus - Plus - Times*) (* Simplify times*)
+                      | Term(m, n) -> (* Plus - Plus - Term *) (* Flatten *)
+                        let l = el@tl::li in
+                        let l = sort_by_degree l in
+                          Plus(l)
+                      | Plus(el2) -> (* Plus - Plus - Plus *) (* Flatten *)
+                        let l = el@el2@li in
+                        let l = sort_by_degree l in
+                          Plus(l)
+                      | Times(el2) -> (* Plus - Plus - Times*) (* Simplify times and flatten*)
                         let tl = simplify1 tl in
-                          Plus(hd::tl::li)
+                        let l = el@tl::li in
+                        let l = sort_by_degree l in
+                          Plus(l)
                     )
 
                   | Times(el) ->
                     (
                       match tl with
-                        | Term(m, n) -> (* Plus - Times - Term : (4*x)+x *)
+                        | Term(m, n) -> (* Plus - Times - Term *) (* Simplify times *)
                           let hd = simplify1 hd in
-                            Plus(hd::tl::li)
-                        | Plus(el2) -> (* Plus - Times - Plus *)
+                          let l = hd::tl::li in
+                          let l = sort_by_degree l in
+                            Plus(l)
+                        | Plus(el2) -> (* Plus - Times - Plus *) (* Simplify times and flatten*)
                           let hd = simplify1 hd in
-                            Plus(hd::el2@li)
-                        | Times(el2) -> (* Plus - Times - Times *)
+                          let l = hd::el2@li in
+                          let l = sort_by_degree l in
+                            Plus(l)
+                        | Times(el2) -> (* Plus - Times - Times *) (* Simplify both times *)
                           let hd = simplify1 hd in
                           let tl = simplify1 tl in
-                            Plus(hd::tl::li)
+                          let l = hd::tl::li in
+                          let l = sort_by_degree l in
+                            Plus(l)
                     )
               )
             | _ -> e
@@ -257,34 +327,37 @@ let rec simplify1 (e:pExp): pExp =
               | Term(m1, n1) ->
                 (
                   match tl with
-                    | Term(m2, n2) -> (* Times - Term - Term : 4x * 6 *)
+                    | Term(m2, n2) -> (* Times - Term - Term *) (* Multiply terms, return single term or times list *)
                       let t = Term(m1 * m2, n1 + n2) in
                       (
                         match li with
                           | [] -> t
                           | _ -> Times(t::li)
                       )
-                    | Plus(el) -> (* Times - Term - Plus : 4x * (3 + 5x) *) (* Expand commutative *)
+                    | Plus(el) -> (* Times - Term - Plus  *) (* Expand distributive_term *)
                         let l = [] in
-                        let c = commutative hd el l in
+                        let c = distributive_term hd el l in
+                        let c = sort_by_degree c in
                         Plus(c)
-                    | Times(el) -> (* Times - Term - Times : 4x^2 * (4 * 5x) *) (* Simplify tail and recurse, TODO - flatten *)
-                      let p = Times(el) in
-                      let s = simplify1 p in
-                      let e2 = Times(hd::s::li) in
-                        simplify1 e2
+                    | Times(el) -> (* Times - Term - Times *) (* Flatten *)
+                        Times(hd::el@li)
                 )
               | Plus(el) ->
                 (
                 match tl with
-                  | Term(m, n) -> (* Times - Plus -Term : (4+5x) * 3x *) (* Expand commutative *)
+                  | Term(m, n) -> (* Times - Plus -Term *) (* Expand distributive_term *)
                       let l = [] in
-                      let c = commutative tl el l in
+                      let c = distributive_term tl el l in
+                      let c = sort_by_degree c in
                       Plus(c)
-                  | Plus(el) -> (* Times - Plus - Plus : (5 +3x)*(2+x+3x^2) *)
-                      tl
-                  | Times(el) -> (* Times - Plus - Times : (4+x)*(4*x^2) *)
-                      tl
+                  | Plus(el2) -> (* Times - Plus - Plus *) (* Expand distibutive_plus *)
+                      let l = [] in
+                      let l = distributive_plus el el2 l in
+                      let l = sort_by_degree l in
+                      Plus(l)
+                  | Times(el) -> (* Times - Plus - Times *) (* Simplify Times *)
+                      let tl = simplify1 tl in
+                        Times(hd::tl::li)
                 )
 
               | Times(el) ->
@@ -292,10 +365,10 @@ let rec simplify1 (e:pExp): pExp =
                 match tl with
                   | Term(m, n) -> (* Times - Times - Term *) (* Flatten *)
                       Times(el@tl::li)
-                  | Plus(el) -> (* Times - Times - Plus : (5 +3x)*(2+x+3x^2) *)
+                  | Plus(el) -> (* Times - Times - Plus *) (* Simplify Times *)
                       let hd = simplify1 hd in
                       Times(hd::tl::li)
-                  | Times(el2) -> (* Times - Times - Times : (4+x)*(4*x^2) *) (* Flatten *)
+                  | Times(el2) -> (* Times - Times - Times *) (* Flatten *)
                       Times(el@el2@li)
                 )
             )
@@ -309,7 +382,6 @@ let rec simplify1 (e:pExp): pExp =
   Compute if two pExp are the same 
   Make sure this code works before you work on simplify1  
 *)
-(* TODO - test*)
 let equal_pExp (_e1: pExp) (_e2: pExp) :bool =
   if(_e1 = _e2) then
     true
@@ -326,6 +398,7 @@ let equal_pExp (_e1: pExp) (_e2: pExp) :bool =
 let rec simplify (e:pExp): pExp =
     let rE = simplify1(e) in
       print_pExp rE;
+      Printf.printf("\n");
       if (equal_pExp e rE) then
         e
       else
